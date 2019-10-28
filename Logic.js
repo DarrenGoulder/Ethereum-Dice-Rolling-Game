23886f40d0572ccbcd33895b0f4bf8d828eb3de9
DarrenGoulder <37029897+DarrenGoulder@users.noreply.github.com>
2019-10-28 00:40:33 +0000

    Updated HTML & Javascript.

--- Logic.js
+++ Logic.js
@@ -1,53 +1,73 @@
 
-var randomNo1 = 0,
-randomNo2 = 0,
-scrDirectory1 ="",
-scrDirectory2 = "",
-diceResult = 0,
-resultText ="";
+var randomNo1, randomNo2;
+var scrDirectory1, scrDirectory2;
+var diceResult, resultText, i;
+var evenSelected;
 
+var thumbs = document.getElementById("bE");
 
-var button1 = document.getElementById("rollButtonEven");
-var button2 = document.getElementById("rollButtonOdd");
+thumbs.addEventListener("click", evenFunction);
 
-button1.onclick = function (){
+document.getElementById("bE").addEventListener("click", evenFunction);
 
-    randomNo1 = Math.floor((Math.random() * 6) + 1); //Random no generator from 1-6.
-    randomNo2 = Math.floor((Math.random() * 6) + 1); //Random no generator from 1-6.
-    //return randomNo1;
-    //return randomNo2;
+rollDice();
 
-  scrDirectory1 = "https://github.com/DarrenGoulder/Ethereum-Dice-Rolling-Game/blob/master/images/Black" + randomNo1 + ".png?raw=true";
-  scrDirectory2 = "https://github.com/DarrenGoulder/Ethereum-Dice-Rolling-Game/blob/master/images/Red" + randomNo2 + ".png?raw=true";
-  document.getElementById("die1").src = scrDirectory1;
-  document.getElementById("die2").src = scrDirectory2;
-  diceResult = randomNo1 + randomNo2;
-  if(diceResult%2 == 0) {
-    resultText = " - EVEN YOU WIN!!";
-  }
-  else resultText = " - ODD YOU LOOSE!!";
-
-  document.getElementById("result").innerHTML = "Result is "+ diceResult + resultText;
-
+function evenFunction() {
+  loopDiceRoll();
+  document.getElementById("result").innerHTML = "Result is ";
+  setTimeout(executeOutcome(evenSelected), 4500);
 }
 
-button2.onclick = function (){
+function oddFunction() {
+  loopDiceRoll();
+  evenSelected = false;
+  setTimeout(executeOutcome, 4500);
+}
 
-    randomNo1 = Math.floor((Math.random() * 6) + 1); //Random no generator from 1-6.
-    randomNo2 = Math.floor((Math.random() * 6) + 1); //Random no generator from 1-6.
-    //return randomNo1;
-    //return randomNo2;
+function loopDiceRoll() {
+  for (i = 0; i < 15; i++) {
+    setTimeout(rollDice, (i * 300));
+  }
+}
+
+rollDice();
+
+scrDirectory1 = "https://github.com/DarrenGoulder/Ethereum-Dice-Rolling-Game/blob/master/images/Black" + 6 + ".png?raw=true";
+
+function rollDice() {
+
+  randomNo1 = Math.floor((Math.random() * 6) + 1); //Random no generator from 1-6.
+  randomNo2 = Math.floor((Math.random() * 6) + 1); //Random no generator from 1-6.
+  //return randomNo1;
+  //return randomNo2;
 
   scrDirectory1 = "https://github.com/DarrenGoulder/Ethereum-Dice-Rolling-Game/blob/master/images/Black" + randomNo1 + ".png?raw=true";
   scrDirectory2 = "https://github.com/DarrenGoulder/Ethereum-Dice-Rolling-Game/blob/master/images/Red" + randomNo2 + ".png?raw=true";
   document.getElementById("die1").src = scrDirectory1;
   document.getElementById("die2").src = scrDirectory2;
   diceResult = randomNo1 + randomNo2;
-  if(diceResult%2 == 0) {
-    resultText = " - EVEN YOU LOOSE!!";
-  }
-  else resultText = " - ODD YOU WIN!!";
 
-  document.getElementById("result").innerHTML = "Result is "+ diceResult + resultText;
+  document.getElementById("result").innerHTML = "Rolling Dice";
+  return diceResult;
+}
+
+function executeOutcome(diceResult, evenSelected) {
+
+  if (diceResult%2 == 0 & evenSelected == true) {
+  resultText = " - EVEN YOU WIN!!";
+}
+
+  elseif (diceResult%2 == 0 & evenSelected == false); resultText = " - EVEN YOU LOOSE!!";
+  }
+
+  elseif(diceResult%2 != 0); {
+  resultText = " - ODD YOU WIN!!";
+  }
+
+  else () {
+    resultText = " - ODD YOU LOOSE!!";
+  }
+
+document.getElementById("result").innerHTML = "Result is "+ diceResult + resultText;
 
 }
